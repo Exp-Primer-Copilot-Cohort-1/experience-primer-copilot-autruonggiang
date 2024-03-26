d118b2598ece9914ebd0d66f807a7c45a9eb047c
James Au <21522019@gm.uit.edu.vn>
2024-03-26 16:26:15 +0000

    Copilot second commit

---
+++ member.js
@@ -0,0 +1,66 @@
+function skillsMember() {
+    var skills = [
+        {
+            name: 'JavaScript',
+            level: '90%'
+        },
+        {
+            name: 'HTML',
+            level: '90%'
+        },
+        {
+            name: 'CSS',
+            level: '90%'
+        },
+        {
+            name: 'React',
+            level: '80%'
+        },
+        {
+            name: 'Redux',
+            level: '80%'
+        },
+        {
+            name: 'Node.js',
+            level: '70%'
+        },
+        {
+            name: 'MongoDB',
+            level: '70%'
+        },
+        {
+            name: 'Express',
+            level: '70%'
+        },
+        {
+            name: 'Python',
+            level: '60%'
+        },
+        {
+            name: 'Java',
+            level: '60%'
+        },
+        {
+            name: 'C++',
+            level: '50%'
+        },
+        {
+            name: 'C',
+            level: '50%'
+        },
+        {
+            name: 'SQL',
+            level: '50%'
+        },
+        {
+            name: 'Django',
+            level: '50%'
+        },
+        {
+            name: 'Flask',
+            level: '50%'
+        },
+    ];
+
+    return skills;
+}
